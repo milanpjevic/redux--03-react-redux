e550c42f27a1be9307e040b9359cbaccda8c5c63
milanpjevic <milanpjevic@gmail.com>
2020-10-18 14:11:02 +0200

    Connecting React to Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import App from "./App";
+import reducer from "./store/reducer";
 
 const store = createStore(reducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,9 @@
 const initialState = {
-    counter: 0
-}
+	counter: 0,
+};
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    return state;
+	return state;
 };
 
 export default reducer;
